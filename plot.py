2b1fd4123fd05273cf236b9ca0b59237297f201c
Sam K <sam.d.killen@gmail.com>
2026-06-11 05:01:36 +1000

    more scripts

--- plot.py
+++ plot.py
@@ -6,24 +6,38 @@ df = pd.read_csv(sys.argv[1])
 written = 500_000_000
 df["speed"] = df.apply(lambda row: written / row["mean"] , axis=1)
 
-pivot = df.pivot(
+speed_pivot = df.pivot(
     index="parameter_len",
     columns="parameter_memset",
     values="speed",
 )
+time_pivot = df.pivot(
+    index="parameter_len",
+    columns="parameter_memset",
+    values="mean",
+)
 
-ax = pivot.plot(marker="o")
+ax = speed_pivot.plot(marker="o")
 ax.set_title("Write Length")
 ax.set_xlabel("Write Length")
-ax.set_ylabel("GB / Second")
 
 ax.set_xscale("log", base=2)
-ax.set_xticks(pivot.index)
-ax.set_xticklabels([str(x) for x in pivot.index])
+ax.set_xticks(speed_pivot.index)
+ax.set_xticklabels([str(x) for x in speed_pivot.index])
 
-yticks = [0, 0.5, 1, 1.5, 2, 2.5]
-ax.set_yticks([i * 1_000_000_000 for i in yticks])
-ax.set_yticklabels([str(x)  for x in yticks])
+ax.set_ylabel("GB / Second")
+ax_yticks = [0, 0.5, 1, 1.5, 2, 2.5]
+ax.set_yticks([i * 1_000_000_000 for i in ax_yticks])
+ax.set_yticklabels([str(x)  for x in ax_yticks])
+
+ax2 = time_pivot.plot(marker="o")
+ax2.set_title("Write Length")
+ax2.set_xlabel("Write Length")
+
+ax2.set_xscale("log", base=2)
+ax2.set_xticks(time_pivot.index)
+ax2.set_xticklabels([str(x) for x in time_pivot.index])
+
 
 ax.grid(True, which="both")  # optional: grid for log scale
 plt.show()
---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt install hyperfine unzip vim -y
+wget https://ziglang.org/builds/zig-x86_64-linux-0.17.0-dev.813+2153f8143.tar.xz
+# or
+# wget https://ziglang.org/builds/zig-aarch64-linux-0.17.0-dev.813+2153f8143.tar.xz
+
+
+wget https://github.com/skk64/zig-memset-bench/archive/refs/heads/main.zip
+
+tar -xvf zig-x86_64-linux-0.17.0-dev.813+2153f8143.tar.xz
+unzip main.zip
+
+
