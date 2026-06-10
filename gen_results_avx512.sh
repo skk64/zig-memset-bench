9898a751479ceefce6e7fd7e57e90a1f517b6c63
Sam K <sam.d.killen@gmail.com>
2026-06-11 06:13:55 +1000

    builtin libc - bench script

--- gen_results_avx512.sh
+++ gen_results_avx512.sh
@@ -1,5 +1,5 @@
 hyperfine --warmup 3 \
--L memset glibc_avx2,glibc_avx512,skk64,rpkak,builtin,musl_asm,basic \
+-L memset glibc_avx512,glibc_avx2,libc,skk64,rpkak,builtin,musl_asm,basic \
 -L len 1,2,3,4,5,6,7,8,9,10,12,14,16,18,20,24,28,32,50,100,200,400,800 \
 './zig-out/bin/memset_bench {memset} 500_000_000 {len}' \
 --export-csv results.csv
